fec8f130b21dbb8cf62b61324feb5a48dd1cea28
Jordan Duran <46588887+jordanduran@users.noreply.github.com>
2026-09-14 13:52:08 -0400

    Add collapsible window around producer globe

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,7 @@ import "./responsive-tuning.css";
 import "./pack-storefront.css";
 import "./pack-explorer.css";
 import "./pack-color.css";
+import "./atlas-window.css";
 const sans = Geist({ subsets: ["latin"], variable: "--font-geist-sans" });
 const mono = Geist_Mono({ subsets: ["latin"], variable: "--font-geist-mono" });
 export const metadata: Metadata = {
